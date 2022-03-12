ba38bc4e96a60d1501591b4069d186e64aa3f576
Neal Robben <neal.robben@gmail.com>
2022-03-12 14:12:38 +0100

    Updated configuration

--- BlazingTrails.Api/Program.cs
+++ BlazingTrails.Api/Program.cs
@@ -3,20 +3,23 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
