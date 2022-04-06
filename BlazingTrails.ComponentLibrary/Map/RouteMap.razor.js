01f5b0bc52cc2ca02ea919e5ea116fd27d48bb2c
Neal Robben <neal.robben@gmail.com>
2022-04-06 23:10:35 +0200

    Invoke WaypointAdded from javascript whenever a waypoint is added

--- BlazingTrails.ComponentLibrary/Map/RouteMap.razor.js
+++ BlazingTrails.ComponentLibrary/Map/RouteMap.razor.js
@@ -1,4 +1,4 @@
-export function initialize(hostElement) {
+export function initialize(hostElement, routeMapComponent) {
 
     hostElement.map = L.map(hostElement).setView([51.700, -0.10], 3);
 
@@ -18,6 +18,8 @@ export function initialize(hostElement) {
         let line = L.polyline(hostElement.waypoints.map(m => m.getLatLng()),
         { color: 'var(--brand)' }).addTo(hostElement.map);
         hostElement.lines.push(line);
+
+        routeMapComponent.invokeMethodAsync('WaypointAdded', e.latlng.lat, e.latlng.lng);
     });
 }
 
